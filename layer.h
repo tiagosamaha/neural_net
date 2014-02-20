6a2525ed5d11257ba115f762b612cc22372ef2b9
Tiago Samaha <tiagosamaha@gmail.com>
2014-02-20 13:37:26 -0300

    create neurons at the layer

--- layer.h
+++ layer.h
@@ -5,12 +5,15 @@ using namespace std;
 class Layer {
   private:
     int size;
-    Neuron *neurons;
+    Neuron **neurons;
     
   public:
     Layer(int number_of_neurons, int inputs_per_neuron) {
       size = number_of_neurons;
-      neurons = new Neuron[size];
+      neurons = new Neuron*[size];
+      for (int i=0; i<size; i++) {
+        neurons[i] = new Neuron(inputs_per_neuron);
+      }
     }
     
     ~Layer() {
@@ -20,6 +23,11 @@ class Layer {
     int get_size() {
       return size;
     }
+    
+    Neuron* get_neuron(int i) {
+      return neurons[i];
+    }
+
     
     void info() {
       cout << "Neurons: " << get_size() << "\n";
--- main.cpp
+++ main.cpp
@@ -7,20 +7,18 @@
 using namespace std;
 
 int main() {
+  /* Now this is only to test some features */
   srand((unsigned)time(NULL));
   
-  Neuron *n = new Neuron(3);
-  n->show_weights();
-  
   Layer *input_l = new Layer(2, 3);
-  input_l->info();
+  
+  Neuron *x1 = input_l->get_neuron(0);
+  x1->show_weights();
+  
   cout << "\n";
-  Layer *hidden_l = new Layer(3, input_l->get_size());
-  hidden_l->info();
-  cout << "\n";
-  Layer *output_l = new Layer(1, hidden_l->get_size());
-  output_l->info();
-  cout << "\n";
+  
+  Neuron *x2 = input_l->get_neuron(1);
+  x2->show_weights();
   
   return 0;
 }

c5ec31b5d4c990e14acc172be2744eea275f611f
Tiago Samaha <tiagosamaha@gmail.com>
2014-03-12 17:33:52 -0300

    remove unsed file

--- src/functions.h
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-
-double sigmoid(double activation) {
-  return 1 / (1 + exp(-activation));
-}
-
-double sigmoid_derivative(double activation) {
-  return sigmoid(activation) * (1 - sigmoid(activation));
-}

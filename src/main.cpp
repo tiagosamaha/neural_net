bf53c916824c60fe7a447ddd0f469ed3e0bcb9a5
Tiago Samaha <tiagosamaha@gmail.com>
2014-03-12 17:34:02 -0300

    change example

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,16 @@ using namespace std;
 
 int main() {
   srand((unsigned)time(NULL));
-  
-  int num_input = 9;
-  int num_hidden = 2;
-  int hidden_neurons[] = {5,4};
+
+  int input[] = {1,1};
+  int num_input = 2;
+  int num_hidden = 1;
+  int hidden_neurons[] = {1};
   int num_output = 2;
   
   MLP *nn = new MLP(num_input, num_hidden, hidden_neurons, num_output);
+  nn->feedforward(input);
   
-  return 0;
+  return 0;  
 }
 

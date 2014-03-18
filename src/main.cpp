bdc64eafada260952598a38293dc86eac5aa577f
Tiago Samaha <tiagosamaha@gmail.com>
2014-03-18 14:11:58 -0300

    fix indent

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,17 @@
 using namespace std;
 
 int main() {
-  srand((unsigned)time(NULL));
-
-  int input[] = {1,1};
-  int num_input = 2;
-  int num_hidden = 1;
-  int hidden_neurons[] = {1};
-  int num_output = 2;
-  
-  MLP *nn = new MLP(num_input, num_hidden, hidden_neurons, num_output);
-  nn->feedforward(input);
-  
-  return 0;  
+    srand((unsigned)time(NULL));
+    
+    int input[] = {1,1};
+    int num_input = 2;
+    int num_hidden = 1;
+    int hidden_neurons[] = {1};
+    int num_output = 2;
+     
+    MLP *nn = new MLP(num_input, num_hidden, hidden_neurons, num_output);
+    nn->feedforward(input);
+    
+    return 0;
 }
 
--- src/neuron.h
+++ src/neuron.h
@@ -4,60 +4,60 @@
 using namespace std;
 
 class Neuron {
-  private:
-    int input_size;
-    double *weights;
-    double bias; // bias weight
-    double activation;
-    double output;
+    private:
+        int input_size;
+        double *weights;
+        double bias; // bias weight
+        double activation;
+        double output;
 
-  public:
-    Neuron() {};
+    public:
+        Neuron() {};
     
-    Neuron(int number_of_inputs) {
-      input_size = number_of_inputs;
-      weights = new double[input_size];
-      for (int i=0; i<input_size; i++) {
-        weights[i] = ((double) rand() / (RAND_MAX));
-      }
-      bias = ((double) rand() / (RAND_MAX));
-    }
+        Neuron(int number_of_inputs) {
+            input_size = number_of_inputs;
+            weights = new double[input_size];
+            for (int i=0; i<input_size; i++) {
+                weights[i] = ((double) rand() / (RAND_MAX));
+            }
+            bias = ((double) rand() / (RAND_MAX));
+        }
     
-    ~Neuron() {
-      delete [] weights;
-    }
+        ~Neuron() {
+            delete [] weights;
+        }
     
-    int get_size() {
-        return input_size;
-    }
+        int get_size() {
+            return input_size;
+        }
     
-    double get_activation() {
-        return activation;
-    }
-    
-    double get_bias() {
-        return bias;
-    }
-    
-    double get_output() {
-        return output;
-    }
-    
-    double get_weight(int index) {
-      return weights[index];
-    }
-    
-    void set_activation(double value) {
-      activation = value;
-    }
-    
-    void set_output(double value) {
-      output = value;
-    }
-    
-    void show_weights() {
-      for (int i=0; i<input_size; i++)
-        cout << weights[i] << "\n";
-    }
+        double get_activation() {
+            return activation;
+        }
+        
+        double get_bias() {
+            return bias;
+        }
+        
+        double get_output() {
+            return output;
+        }
+        
+        double get_weight(int index) {
+            return weights[index];
+        }
+        
+        void set_activation(double value) {
+            activation = value;
+        }
+        
+        void set_output(double value) {
+            output = value;
+        }
+        
+        void show_weights() {
+            for (int i=0; i<input_size; i++)
+                cout << weights[i] << "\n";
+        }
 };
 
